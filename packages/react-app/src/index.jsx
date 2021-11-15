6f20541f8532f6a188eb4d072dfc787aa3cf8aa3
David In <din2@jhu.edu>
2021-11-15 16:53:07 +0900

    added password protection

--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -2,6 +2,8 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import ReactDOM from "react-dom";
+import Protect from 'react-app-protect'
+import 'react-app-protect/dist/index.css'
 import App from "./App";
 import "./index.css";
 
@@ -20,10 +22,12 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme || "light"}>
-      <App subgraphUri={subgraphUri} />
-    </ThemeSwitcherProvider>
-  </ApolloProvider>,
+  <Protect sha512='70ABAB289F2603934BD6A623D8E95AB58ACA503560322692FA63DDC7755A63D702F5B318AA876B104ADCEA148086ED93768EF0CFFBB5861CBF7B7540FCDB6DBD'>
+    <ApolloProvider client={client}>
+      <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme || "light"}>
+        <App subgraphUri={subgraphUri} />
+      </ThemeSwitcherProvider>
+    </ApolloProvider>
+  </Protect>,
   document.getElementById("root"),
 );
