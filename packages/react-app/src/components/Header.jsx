8c4e62311a6879b3157c939709f75fd07d223a68
David In <din2@jhu.edu>
2021-12-09 20:35:27 +0900

    finished the pre launch page now lets move to the main page bitchehshshss

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,16 +1,18 @@
 import { PageHeader } from "antd";
 import React from "react";
+import logo from '../static/assets/home-menu.png';
 
 // displays a page header
-
 export default function Header() {
   return (
     <a href="http://ravinggoblins.io/" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="Raving Goblins"
+        title=" "
+        avatar={{ logo }}
         //subTitle="👩‍🔬 Buyer pays to mint NFT example"
         style={{ cursor: "pointer" }}
       />
+      <img className='logo-img' src={logo} alt="logo" align="left"/>
     </a>
   );
 }
--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -1,11 +1,19 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
-import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import ReactDOM from "react-dom";
-import Protect from 'react-app-protect'
+import ReactFullpage from "@fullpage/react-fullpage";
 import 'react-app-protect/dist/index.css'
-import App from "./App";
 import "./index.css";
+import { Statistic, Row, Col } from 'antd';
+import background1 from "./static/assets/background-1.png";
+import background2 from "./static/assets/background-2.png";
+import background3 from "./static/assets/background-3.png";
+import background4 from "./static/assets/background-4.png";
+
+const { Countdown } = Statistic;
+const deadline = Date.parse('19 Dec 2021 15:00:00 GMT');
+
+// This is the index.js file for the Pre-launch Page
 
 const themes = {
   dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
@@ -21,13 +29,79 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const CountdownTimer = () => (
+  <Countdown 
+    title="Let's Meet Again In" 
+    value={deadline} 
+    format="DD:HH:mm:ss:SS"
+    style={{ fontSize: '4em', color: '#FFF', textAlign: 'center', marginBottom: '0.5em'}} 
+  />
+);
+
+class MySection extends React.Component {
+  render() {
+    return (
+      <div 
+        className="section"
+        style={{ 
+          backgroundImage: `url(${this.props.image})`,
+          backgroundPosition: 'center center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover'
+        }}
+      >
+      </div>
+    );
+  }
+}
+
+class MySectionCountdown extends React.Component {
+  render() {
+    return (
+      <div 
+        className="section"
+        style={{ 
+          backgroundImage: `url(${this.props.image})`,
+          backgroundPosition: 'center center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover'
+        }}
+      >
+        <CountdownTimer />
+      </div>
+    );
+  }
+}
+
+const anchors = ["firstPage", "secondPage", "thirdPage", "fourthPage"];
+
+const FullpageWrapper = () => (
+  <ReactFullpage
+    anchors={anchors}
+    navigation
+    navigationTooltips={anchors}
+    sectionsColor={["#0F001C", "#0F001C", "#0F001C", "#0F001C"]}
+    onLeave={(origin, destination, direction) => {
+      console.log("onLeave event", { origin, destination, direction });
+    }}
+    render={({ state, fullpageApi }) => {
+      console.log("render prop change", state, fullpageApi); // eslint-disable-line no-console
+
+      return (
+        <div>
+          <MySection image={background1} />
+          <MySectionCountdown image={background2} />
+          <MySection image={background3} />
+          <MySection image={background4} />
+        </div>
+      );
+    }}
+  />
+);
+
 ReactDOM.render(
-  <Protect sha512='70ABAB289F2603934BD6A623D8E95AB58ACA503560322692FA63DDC7755A63D702F5B318AA876B104ADCEA148086ED93768EF0CFFBB5861CBF7B7540FCDB6DBD'>
-    <ApolloProvider client={client}>
-      <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme || "light"}>
-        <App subgraphUri={subgraphUri} />
-      </ThemeSwitcherProvider>
-    </ApolloProvider>
-  </Protect>,
+  <ApolloProvider client={client}>
+    <FullpageWrapper />
+  </ApolloProvider>,
   document.getElementById("root"),
 );
