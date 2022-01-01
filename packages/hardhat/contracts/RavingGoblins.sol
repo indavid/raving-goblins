6fcf7284b7947a36201c19a0ceb7d5c29add7645
ndaysinaiK <kabulo_nday@kookmin.ac.kr>
2022-01-01 16:08:17 +0900

    modified on 01/01/2022 16:08:16

--- packages/hardhat/contracts/RavingGoblins.sol
+++ packages/hardhat/contracts/RavingGoblins.sol
@@ -14,31 +14,31 @@ contract RavingGoblins is ERC721, Ownable {
   uint256 internal _possibleToMint = 2000;
   uint256 internal price = 0.03 ether; 
 
-  constructor(bytes32[] memory assetsForSale) public ERC721("Raving Goblins ERC721 Token", "RGT") {
+  constructor(/*bytes32[] memory assetsForSale*/) public ERC721("Raving Goblins ERC721 Token", "RGT") {
     _setBaseURI("https://ipfs.io/ipfs/");
-    for(uint256 i=0;i<assetsForSale.length;i++){
+    /*for(uint256 i=0;i<assetsForSale.length;i++){
       forSale[assetsForSale[i]] = true;
-    }
+    }*/
   }
 
   // This marks an item in IPFS as "forsale"
-  mapping (bytes32 => bool) public forSale;
+  //mapping (bytes32 => bool) public forSale;
   // This lets you look up a token by the uri (assuming there is only one of each uri for now)
-  mapping (bytes32 => uint256) public uriToTokenId;
+ // mapping (bytes32 => uint256) public uriToTokenId;
 
   function mintItem(string memory tokenURI)
       public
       payable
       returns (uint256) {
 
-      bytes32 uriHash = keccak256(abi.encodePacked(tokenURI)); 
+      //bytes32 uriHash = keccak256(abi.encodePacked(tokenURI)); 
 
       // Make sure they are only minting something that is marked "forsale"
-      require(forSale[uriHash],"NOT FOR SALE");
+      //require(forSale[uriHash],"NOT FOR SALE");
       require(msg.value >= price);
       require(_possibleToMint>0,"First drop is over");
 
-      forSale[uriHash]=false;
+      //forSale[uriHash]=false;
 
       _tokenIds.increment();
 
@@ -46,7 +46,7 @@ contract RavingGoblins is ERC721, Ownable {
       _mint(msg.sender, id);
       _setTokenURI(id, tokenURI);
 
-      uriToTokenId[uriHash] = id;
+      //uriToTokenId[uriHash] = id;
 
       payable(owner()).transfer(msg.value);
 
@@ -56,15 +56,15 @@ contract RavingGoblins is ERC721, Ownable {
   }
 
   function giveAway(address _team, string memory tokenURI) onlyOwner public returns (uint256) {
-    bytes32 uriHash = keccak256(abi.encodePacked(tokenURI));
-    require(forSale[uriHash],"NOT FOR SALE");
-    forSale[uriHash]=false;
+    //bytes32 uriHash = keccak256(abi.encodePacked(tokenURI));
+    //require(forSale[uriHash],"NOT FOR SALE");
+    //forSale[uriHash]=false;
 
     _tokenIds.increment();
     uint256 id = _tokenIds.current();
     _mint(_team, id);
     _setTokenURI(id, tokenURI);
-    uriToTokenId[uriHash] = id;
+    //uriToTokenId[uriHash] = id;
     return id;
   }
 
